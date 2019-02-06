c402b66e31054b6954ef5d62c2e21974e16f29ea
teamclouday <teamclouday@users.noreply.github.com>
2019-02-06 14:36:18 -0500

    add kernel effects

--- Shaders/SimpleFrag.glsl
+++ Shaders/SimpleFrag.glsl
@@ -4,10 +4,36 @@ out vec4 color;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.0 / 300.0;
+
 void main()
 {
-    color = texture(screenTexture, TexCoords);
-    //float average = (color.r + color.g + color.b) / 3.0;
-    float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
-    color = vec4(average, average, average, 1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset, offset),  // top-left
+        vec2(0.0, offset),      // top-center
+        vec2(offset, offset),   // top-right
+        vec2(-offset, 0.0),     // center-left
+        vec2(0.0, 0.0),         // center-center
+        vec2(offset, 0.0),      // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2(0.0, -offset),     // bottom-center
+        vec2(offset, -offset)   // bottom-right
+    );
+
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col;
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    color = vec4(col, 1.0);
 }
