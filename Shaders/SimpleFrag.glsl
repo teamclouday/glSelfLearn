d421658b5a6a63931e4ad2c77cbc80acbe8922ed
teamclouday <teamclouday@users.noreply.github.com>
2019-02-06 10:22:15 -0500

    add grayscale

--- Shaders/SimpleFrag.glsl
+++ Shaders/SimpleFrag.glsl
@@ -6,5 +6,8 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    color = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+    color = texture(screenTexture, TexCoords);
+    //float average = (color.r + color.g + color.b) / 3.0;
+    float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+    color = vec4(average, average, average, 1.0);
 }
