fa46a95eb9ae1458c33098bdcabc0ba569b5a460
teamclouday <teamclouday@users.noreply.github.com>
2019-02-08 20:00:13 -0500

    add refraction

--- Shaders/DemoFrag.glsl
+++ Shaders/DemoFrag.glsl
@@ -7,10 +7,11 @@ out vec4 color;
 uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
+float ratio = 1.00 / 1.52;
+
 void main()
 {
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 R = refract(I, normalize(Normal), ratio);
     color = texture(skybox, R);
-    color *= 1.5;
 }
