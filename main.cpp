c4cc72a64378f7541a58d2f1954e6762dfd81e5d
teamclouday <teamclouday@users.noreply.github.com>
2019-02-13 17:56:29 -0500

    update

--- main.cpp
+++ main.cpp
@@ -108,7 +108,7 @@ int main()
         glfwPollEvents();
         do_movement();
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
