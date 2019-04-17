324e3a6ee9940f0df6dceb5d22fda4ff4862307e
teamclouday <teamclouday@users.noreply.github.com>
2019-04-17 19:52:45 -0400

    add error functions

---
+++ include/myerror.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR_SDL_INIT              1
+#define ERROR_GLEW_INIT             2
+#define ERROR_SHADER_FILE_NOT_FOUND 3
+#define ERROR_SHADER_COMPILE        4
+#define ERROR_SHADER_PROGRAM_LINK   5
+
+void GLAPIENTRY
+MessageCallback(GLenum source,
+                GLenum type,
+                GLuint id,
+                GLenum severity,
+                GLsizei length,
+                const GLchar* message,
+                const void* userParam);
