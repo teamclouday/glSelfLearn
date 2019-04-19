4d32c676310cf8eb01594f383c5925df69b89d7e
teamclouday <teamclouday@users.noreply.github.com>
2019-04-19 15:11:52 -0400

    add shader storage blocks

--- shaders/simple.vert
+++ shaders/simple.vert
@@ -1,32 +1,25 @@
 #version 450 core
-layout (location = 0) in vec3 position;
+
+struct Vertex
+{
+    vec3 position;
+    vec3 color;
+};
+
+layout (binding=0, std430) buffer my_vertices
+{
+    Vertex vertices[];
+};
+
+uniform mat4 transform_matrix;
 
 out VS_OUT
 {
     vec4 color;
 } vs_out;
 
-uniform mat4 mv_matrix;
-uniform mat4 proj_matrix;
-uniform mat4 scale_matrix;
-
-struct my_structure
-{
-    int a;
-    vec3 b;
-    mat4 c;
-};
-
-layout (binding = 0, std430) buffer my_storage_block
-{
-    vec4 aa;
-    vec3 bb;
-    int cc;
-    my_structure dd;
-};
-
 void main()
 {
-    gl_Position = proj_matrix * mv_matrix * scale_matrix * vec4(position, 1.0);
-    vs_out.color = vec4(position, 1.0) * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);
+    gl_Position = vec4(vertices[gl_VertexID].position, 1.0);
+    vs_out.color = vec4(vertices[gl_VertexID].color, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,37 +12,18 @@ void renderAll()
     glClear(GL_COLOR_BUFFER_BIT);
     glClearBufferfi(GL_DEPTH_STENCIL, 0.0f, 1.0f, 0.0f);
 
-    float currentTime = (float)SDL_GetTicks() / 1000.0f;
-    float tc;
-
-    int window_width, window_height;
-    SDL_GetWindowSize(myWindow, &window_width, &window_height);
-    glm::mat4 proj_matrix = glm::perspective(50.0f, (float)window_width/(float)window_height, 0.1f, 1000.0f);
-    glm::mat4 scale_matrix = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f));
-    glm::mat4 mv_matrix;
+    // float currentTime = (float)SDL_GetTicks() / 1000.0f;
+    
+    glm::mat4 transform_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
 
     myShader->use();
-    
-    glUniformMatrix4fv(glGetUniformLocation(myShader->programID, "proj_matrix"), 1, GL_FALSE, glm::value_ptr(proj_matrix));
-    glUniformMatrix4fv(glGetUniformLocation(myShader->programID, "scale_matrix"), 1, GL_FALSE, glm::value_ptr(scale_matrix));
+    glUniformMatrix4fv(glGetUniformLocation(myShader->programID, "transform_matrix"), 1, GL_FALSE, glm::value_ptr(transform_matrix));
 
     glBindVertexArray(VAO);
 
-    for(int i = 0; i < 500; i++)
-    {
-        tc = (float)i + currentTime * 0.3f;
-        mv_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -20.0f));
-        mv_matrix = glm::translate(mv_matrix, glm::vec3(sinf(2.1f*tc)*2.0f,
-                                                        cosf(1.7f*tc)*2.0f,
-                                                        sinf(1.3f*tc)*cosf(1.5f*tc)*2.0f));
-        mv_matrix = glm::rotate(mv_matrix, currentTime*45.0f*0.1f, glm::vec3(0.0f, 1.0f, 0.0f));
-        mv_matrix = glm::rotate(mv_matrix, currentTime*21.0f*0.1f, glm::vec3(1.0f, 0.0f, 0.0f));
-        glUniformMatrix4fv(glGetUniformLocation(myShader->programID, "mv_matrix"), 1, GL_FALSE, glm::value_ptr(mv_matrix));
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-
-    glBindVertexArray(0);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, buffer);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
     SDL_GL_SwapWindow(myWindow);
 }
@@ -57,58 +38,21 @@ int main(int argc, char *argv[])
     myShader->compile();
 
     glCreateVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
 
-    static const GLfloat vertex_positions[] = {
-        -0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f,  0.5f, -0.5f,
-
-        -0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-
-         0.5f,  0.5f,  0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-
-        -0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f, -0.5f,
-
-        -0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f,  0.5f,  0.5f,
+    static const GLfloat vertex_data[] = {
+        -0.5f,  0.5f, 0.5f, 1.0f,
+        0.0f, 1.0f, 0.3f, 1.0f,
+         0.5f, -0.5f, 0.5f, 1.0f,
+        0.9f, 0.3f, 0.2f, 1.0f,
+         0.5f,  0.5f, 0.5f, 1.0f,
+        0.3f, 1.0f, 0.4f, 1.0f,
     };
 
-    glCreateBuffers(1, &buffer);
-    glNamedBufferStorage(buffer, sizeof(vertex_positions), vertex_positions, 0);
-    glVertexArrayVertexBuffer(VAO, 0, buffer, 0, sizeof(GLfloat)*3);
-    glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
-    glVertexArrayAttribBinding(VAO, 0, 0);
-    glEnableVertexArrayAttrib(VAO, 0);
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, buffer);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(vertex_data), vertex_data, GL_DYNAMIC_COPY);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+    
 
     Uint32 tNow = SDL_GetTicks();
     Uint32 tPrev = SDL_GetTicks();
