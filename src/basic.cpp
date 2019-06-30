845656cef08a68dba01e678b877fc796541faf34
teamclouday <teamclouday@gmail.com>
2019-06-30 16:05:11 +0800

    using compute shaders

--- src/basic.cpp
+++ src/basic.cpp
@@ -4,8 +4,6 @@ extern SDL_Window *myWindow;
 extern SDL_GLContext myContext;
 extern Shader *myShader;
 
-extern bool shot;
-
 void initAll()
 {
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -71,11 +69,6 @@ bool pollEvents()
                     bool isFullScreen = SDL_GetWindowFlags(myWindow) & SDL_WINDOW_FULLSCREEN_DESKTOP;
                     SDL_SetWindowPosition(myWindow, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
                     SDL_SetWindowFullscreen(myWindow, isFullScreen ? 0 : SDL_WINDOW_FULLSCREEN_DESKTOP);
-                    break;
-                }
-                case SDLK_s:
-                {
-                    shot = true;
                     break;
                 }
             }
--- src/main.cpp
+++ src/main.cpp
@@ -5,71 +5,18 @@ SDL_GLContext myContext;
 Shader *myShader;
 GLuint VAO, VBO;
 
-bool shot;
-
-void screenShot()
-{
-    int w, h;
-    SDL_GetWindowSize(myWindow, &w, &h);
-    int row_size = ((w*3+3)&~3);
-    int data_size = row_size * h;
-    unsigned char *data = new unsigned char[data_size];
-
-    #pragma pack (push, 1)
-    struct
-    {
-        unsigned char identsize;    // size of ID field
-        unsigned char cmaptype;     // color map type 0 = none
-        unsigned char imagetype;    // image type 2 = rgb
-        short cmapstart;
-        short cmapsize;
-        unsigned char cmapbpp;
-        short xorigin;
-        short yorigin;
-        short width;
-        short height;
-        unsigned char bpp;
-        unsigned char descriptor;
-    } tga_header;
-    #pragma pack (pop)
-
-    glReadPixels(0, 0, w, h, GL_BGR, GL_UNSIGNED_BYTE, data);
-    memset(&tga_header, 0, sizeof(tga_header));
-    tga_header.imagetype = 2;
-    tga_header.width = (short)w;
-    tga_header.height = (short)h;
-    tga_header.bpp = 24;
-    FILE *f_out = fopen("screenshot.tga", "wb");
-    fwrite(&tga_header, sizeof(tga_header), 1, f_out);
-    fwrite(data, data_size, 1, f_out);
-    fclose(f_out);
-
-    delete[] data;
-}
-
 void renderAll()
 {
     int w, h;
     SDL_GetWindowSize(myWindow, &w, &h);
     glViewport(0, 0, w, h);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(1.0f, 0.6f, 0.4f, 0.0f);
     glClearDepth(1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     myShader->use();
-    glBindVertexArray(VAO);
 
-    glEnable(GL_PROGRAM_POINT_SIZE);
-    glDrawArrays(GL_POINTS, 0, 4);
-
-    glBindVertexArray(0);
     myShader->disuse();
-
-    if(shot)
-    {
-        screenShot();
-        shot = false;
-    }
 
     SDL_GL_SwapWindow(myWindow);
 }
@@ -79,31 +26,8 @@ int main(int argc, char *argv[])
     initAll();
 
     myShader = new Shader();
-    myShader->add("./shaders/point.vert", GL_VERTEX_SHADER);
-    myShader->add("./shaders/point.frag", GL_FRAGMENT_SHADER);
+    myShader->add("./shaders/sample.comp", GL_COMPUTE_SHADER);
     myShader->compile(false);
-
-    const static GLfloat positions[] =
-    {
-         0.5f,  0.5f,
-         0.5f, -0.5f,
-        -0.5f, -0.5f,
-        -0.5f,  0.5f,
-    };
-
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), (GLvoid*)NULL);
-    glEnableVertexAttribArray(0);
-
-    glBindVertexArray(0);
-
-    shot = false;
 
     printf("%s\n", glGetString(GL_RENDERER));
 
